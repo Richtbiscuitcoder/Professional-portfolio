0df009bda72bbb6444b7cee2ece16548fa0e27f3
Rich <richowen87@icloud.com>
2024-04-16 10:59:44 +0100

    working modals.. on to json

--- index.js
+++ index.js
@@ -2,18 +2,20 @@
 
 const modalOne = document.querySelector('.modal');
 const openModal = document.querySelector('.open-modal');
-const closeModal = document.querySelector('.close-modal');
+const closeModal = document.querySelector('#modal');
 
 
 /*
 openModal.addEventListener('click', ()=>{
     modalOne.showModal();
 })
+*/
 
 closeModal.addEventListener('click', ()=>{
     modalOne.close();
 })
-*/
+
+
 
 let btns = document.querySelectorAll('button');
 
@@ -21,5 +23,6 @@ btns.forEach(function (e) {
     e.addEventListener('click', function(){
         modalOne.showModal();
     })
+    
+    
 })
-
