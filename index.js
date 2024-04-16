bfedea126e659e1462083f4010786dc5d0f90e44
Rich <richowen87@icloud.com>
2024-04-16 14:04:51 +0100

    everything working at this point

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
 
-
+//modal button functions to open and close 
 const modalOne = document.querySelector('.modal');
 const openModal = document.querySelector('.open-modal');
 const closeModal = document.querySelector('#modal');
 
 
-/*
-openModal.addEventListener('click', ()=>{
-    modalOne.showModal();
-})
-*/
+
+
 
 closeModal.addEventListener('click', ()=>{
     modalOne.close();
 })
 
 
+// for the json info
+ 
+fetch('index.json')
+.then((response) => {
+    return response.json()
+}) .then((data) => {
 
-let btns = document.querySelectorAll('button');
-
-btns.forEach(function (e) {
-    e.addEventListener('click', function(){
-        modalOne.showModal();
-    })
-    
-    
+data.projects.forEach(project => {
+    document.querySelector('.grid-container').innerHTML += `
+    <div class="project-${project.id}">
+        <button class="project-btn open-modal" id="${project.id}" title="button" data-title="${project.title}" data-id="${project.id}" data-description="${project.description} data-image="${project.image}">Learn more</button>
+    </div>
+    `
 })
+
+document.querySelectorAll('.grid-container button').forEach(button => {
+    button.addEventListener('click', (e) => {
+        modalOne.showModal();
+        console.log(e.target.dataset.title)
+    })
+})
+
+
+ 
+})
+
