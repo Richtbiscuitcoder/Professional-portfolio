902a550cb02d89a02904ff8490664c646ad46ec9
Rich <richowen87@icloud.com>
2024-04-17 10:17:16 +0100

    updated read me and project tiles

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Professional-portfolio
+
+My first project at iO Academy's software developer essentials course.
--- index.js
+++ index.js
@@ -15,9 +15,9 @@ closeModal.addEventListener('click', ()=>{
 
 // for the json info
 const index = 0
-function makeModalContent(id, title, description) {
+function makeModalContent(image, title, description) {
     document.querySelector('.output').innerHTML = `
-    <li>${id}</li>
+    <li>${image}</li>
     <li>${title}</li>
     <li>${description}</li>
     <button class="close-btn close-modal">&#x2715; Close</button>
@@ -33,8 +33,10 @@ fetch('index.json')
 data.projects.forEach(project => {
     document.querySelector('.grid-container').innerHTML += `
     <div class="project-${project.id}">
-        <p>${project.title}<p>
-        <button class="project-btn open-modal" id="${project.id}" title="button" data-title="${project.title}" data-id="${project.id}" data-description="${project.description} data-image="${project.image}" data-url="${project.url}">Learn more</button>
+        <h3>${project.title}</h3>
+        <img src=${project.image}>
+        <a rel="noopener" href="${project.url}" target="_blank" title="github"><i class="fa-brands fa-github"></i>  view on Github</a>
+        <button class="project-btn open-modal" id="${project.id}" title="button" data-title="${project.title}" data-id="${project.id}" data-description="${project.description}" data-image="${project.image}" data-url="${project.url}">Learn more</button>
     </div>
     `
 })
@@ -42,7 +44,7 @@ data.projects.forEach(project => {
 document.querySelectorAll('.grid-container button').forEach(button => {
     button.addEventListener('click', (e) => {
         modalOne.showModal();
-        makeModalContent(button.dataset.id, button.dataset.title, button.dataset.description)
+        makeModalContent(button.dataset.image, button.dataset.title, button.dataset.description)
     })
 
 
