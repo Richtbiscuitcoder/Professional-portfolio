7ea9b43603325633503b6ae5efdef617fde09ba3
Rich <richowen87@icloud.com>
2024-04-19 11:24:05 +0100

    fonts and final touches

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function makeModalContent(url, title, description) {
     document.querySelector('.output').innerHTML = `
     <span>${title}</span>
     <p>${description}</p>
-    <a>View Live</a>
+    <a rel="noopener" href="google.com" target="_blank">View Live</a>
     <a rel="noopener" href="${url}" target="_blank" title="github"><i class="fa-brands fa-github"></i>view on Github</a>
     <button class="close-btn close-modal">&#x2715; Close</button>
        `
@@ -32,7 +32,7 @@ data.projects.forEach(project => {
         <h3>${project.title}</h3>
         <img src=${project.image} alt="project image displayed with info about it">
         <a rel="noopener" href="${project.url}" target="_blank" title="github"><i class="fa-brands fa-github"></i>  view on Github</a>
-        <a>View Live</a>
+        <a rel="noopener" href="google.com" target="_blank">View Live</a>
         <button class="project-btn open-modal" id="${project.id}" title="button" data-title="${project.title}" data-id="${project.id}" data-description="${project.description}" data-image="${project.image}" data-url="${project.url}">Learn more</button>
     </div>
     `
